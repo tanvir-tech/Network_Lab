033659b600931e4144eda096d9665f34c7a72877
tanvirtech <tanvirah325@gmail.com>
2021-01-23 23:08:05 +0600

    get interface ip by name

--- src/Lab-02_Socket/local_machine_info.py
+++ src/Lab-02_Socket/local_machine_info.py
@@ -1,8 +1,8 @@
 import socket
 
 def print_machine_info():
-    #host_name = socket.gethostname()
-    host_name = "www.rmit.edu.au"
+    host_name = socket.gethostname()
+    #host_name = "www.rmit.edu.au"
     ip_address = socket.gethostbyname(host_name)
     print (" Host name: %s" % host_name)
     print (" IP address: %s" % ip_address)
